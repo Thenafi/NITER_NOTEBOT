11f881b79702d4e4686acb86e8d9fe43bdca9170
Thenafi <65831153+Thenafi@users.noreply.github.com>
2022-02-18 20:18:47 +0600

    scraped

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ import re
 import json
 import time
 from random import randrange
+from unicodedata import name
 from selenium import webdriver
 from airbnb import Api
 from urllib.parse import urlparse
@@ -43,12 +44,46 @@ for url in urls:
             driver.get(url)
             driver.find_element_by_xpath('//*[@id="t3-content"]/div/article/section/table')
         except:
-            pass
+            try:
+                driver.get(url)
+                driver.find_element_by_xpath('//*[@id="t3-content"]/div/article/section/table')
+            except:
+                pass
     except:
         continue
+
+    # here we are searching for name 
+    try:
+        try:
+            namee = driver.find_element_by_xpath('//table/tbody/tr[2]//p/span[@style="color: #000000; font-size: 18pt;"]').text
+        except:
+            try:
+                namee = driver.find_element_by_xpath('//table/tbody/tr[2]//p/span[@style="font-size: 18pt; color: #000000;"]').text
+            except:
+                try: 
+                    namee = driver.find_element_by_xpath('//table/tbody/tr[2]//p//span[@style="font-size: 18pt;"]').text
+                except:
+                    namee = driver.find_element_by_xpath('//*[@id="t3-content"]/div/article/header/h1/a').text
+
+    except:     
+        namee  = "No Name"
+
+    # finding number
+    number = driver.find_element_by_xpath ('//td//*[contains(text(),"1")]/ancestor::td').text.split(", ")
+    
+    
+
+    #finding email
+    email = driver.find_element_by_xpath ('//td//*[contains(text(),"@")]/ancestor::td').text.split(", ")
+    if email[0].find("\n"):
+        email = driver.find_element_by_xpath ('//td//*[contains(text(),"@")]/ancestor::td').text.split("\n")
+        
+
     teacher = {
-        "name" : driver.find_element_by_xpath('//table/tbody/tr[2]//p/span[@style]').text,
-        "Number" : driver.find_element_by_xpath ('//table/tbody/tr[5]/td[2]').text
+        "url ": url,
+        "name" : namee,
+        "number" : number,
+        "email" : email
 
     }
     data.append(teacher)
